bd0ddc442debbc92293703fa56b2274f14676f3d
Ramesh S <92824874+rameshmdm@users.noreply.github.com>
2022-08-26 21:50:00 +0530

    onTap function updated
    onTap function updated

--- re_usable_buttons/lib/main.dart
+++ re_usable_buttons/lib/main.dart
@@ -33,50 +33,86 @@ class MyHomePage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const ReUsableIconButton(buttonColor: Colors.blue, icon: Icons.add_box_rounded),
-              const ReUsableIconButton(buttonColor: Colors.pink, icon: Icons.remove_circle),
-              const ReUsableIconButton(
+              ReUsableIconButton(
+                buttonColor: Colors.blue,
+                icon: Icons.add_box_rounded,
+                onTap: () async {
+                  await Future.delayed(const Duration(milliseconds: 100));
+                },
+              ),
+              ReUsableIconButton(
+                buttonColor: Colors.pink,
+                icon: Icons.remove_circle,
+                onTap: () async {
+                  await Future.delayed(const Duration(milliseconds: 100));
+                },
+              ),
+              ReUsableIconButton(
                 buttonColor: Colors.green,
                 icon: Icons.notifications_active,
                 iconSize: 30,
                 borderRadius: 5,
                 verticalPadding: 10,
                 horizontalPadding: 60,
+                onTap: () async {
+                  await Future.delayed(const Duration(milliseconds: 100));
+                },
               ),
-              const ReUsableIconButton(
+              ReUsableIconButton(
                 buttonColor: Colors.black,
                 icon: Icons.mail,
                 iconSize: 30,
                 borderRadius: 20,
                 verticalPadding: 10,
                 horizontalPadding: 60,
+                onTap: () async {
+                  await Future.delayed(const Duration(milliseconds: 100));
+                },
               ),
-              const ReUsableIconButton(
+              ReUsableIconButton(
                 buttonColor: Colors.teal,
                 icon: Icons.add_circle,
                 iconSize: 40,
                 borderRadius: 30,
                 verticalPadding: 10,
                 horizontalPadding: 60,
+                onTap: () async {
+                  await Future.delayed(const Duration(milliseconds: 100));
+                },
               ),
-              const ReUsableIconButton(buttonColor: Colors.blueAccent, icon: Icons.cached_rounded),
+              ReUsableIconButton(
+                buttonColor: Colors.blueAccent,
+                icon: Icons.cached_rounded,
+                onTap: () async {
+                  await Future.delayed(const Duration(milliseconds: 100));
+                },
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
+                children: [
                   ReUsableIconButton(
                     buttonColor: Colors.deepOrangeAccent,
                     icon: Icons.add_shopping_cart_outlined,
                     iconSize: 40,
+                    onTap: () async {
+                      await Future.delayed(const Duration(milliseconds: 100));
+                    },
                   ),
                   ReUsableIconButton(
                     buttonColor: Colors.cyan,
                     icon: Icons.add_shopping_cart_outlined,
                     iconSize: 40,
+                    onTap: () async {
+                      await Future.delayed(const Duration(milliseconds: 100));
+                    },
                   ),
                   ReUsableIconButton(
                     buttonColor: Colors.purpleAccent,
                     icon: Icons.add_shopping_cart_outlined,
                     iconSize: 40,
+                    onTap: () async {
+                      await Future.delayed(const Duration(milliseconds: 100));
+                    },
                   ),
                 ],
               ),
--- re_usable_buttons/lib/reusablebutton.dart
+++ re_usable_buttons/lib/reusablebutton.dart
@@ -9,18 +9,20 @@ class ReUsableIconButton extends StatelessWidget {
   final double? horizontalPadding;
   final double? verticalPadding;
   final double? borderRadius;
+  final Function() onTap;
 
-  const ReUsableIconButton(
-      {Key? key,
-      required this.buttonColor,
-      this.splashColor = Colors.white,
-      this.highlightedColor,
-      required this.icon,
-      this.iconSize = 20,
-      this.horizontalPadding = 20,
-      this.verticalPadding = 20,
-      this.borderRadius = 40})
-      : super(key: key);
+  const ReUsableIconButton({
+    Key? key,
+    required this.buttonColor,
+    this.splashColor = Colors.white,
+    this.highlightedColor,
+    required this.icon,
+    this.iconSize = 20,
+    this.horizontalPadding = 20,
+    this.verticalPadding = 20,
+    this.borderRadius = 40,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -29,9 +31,7 @@ class ReUsableIconButton extends StatelessWidget {
       child: InkWell(
         splashColor: splashColor,
         highlightColor: highlightedColor,
-        onTap: () async {
-          await Future.delayed(const Duration(milliseconds: 100));
-        },
+        onTap: onTap,
         borderRadius: BorderRadius.all(Radius.circular(borderRadius!)),
         child: Ink(
           padding: EdgeInsets.symmetric(horizontal: horizontalPadding!, vertical: verticalPadding!),
